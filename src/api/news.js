147dbecfcdfa969504d1e1422be9d8ea61c223ae
Nikhilnair48 <nikhil.nair48@gmail.com>
2020-06-20 20:28:03 +0530

    Charts, Pagination and Event Handling  - Hacker News results filtering for popular news  - Functionality: hide and upvote news  - Integrated redux-persist for hidden and upvoted news  - react-charts integration to showcase upvotes for news IDs  - Realtime update of charts based on user events

--- src/api/news.js
+++ src/api/news.js
@@ -1,6 +1,7 @@
 const fetchNews = async (params) => {
   const response = await fetch(
-    `https://hn.algolia.com/api/v1/search_by_date?query=...&page=${params.page}&numericFilters=points>10`,
+    `https://hn.algolia.com/api/v1/search_by_date?query=...&page=${params.page}&numericFilters=points%3E
+    10`,
     {
       method: "GET"
     }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,32 @@
 import { combineReducers } from "redux";
-import news from "./news";
+import { newsReducer, upvotedReducer, hideReducer } from "./news";
 import chart from "./chart";
+import {
+  persistStore,
+  persistReducer,
+  persistCombineReducers
+} from "redux-persist";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
+
+const storiesPersistConfig = {
+  key: "stories",
+  storage: storage
+};
+
+const upvotedReducerPersistConfig = {
+  key: "upvotedReducer",
+  storage: storage
+};
+const hideReducerPersistConfig = {
+  key: "hideReducer",
+  storage: storage
+};
 
 const rootReducer = combineReducers({
-  news: news
+  stories: persistReducer(storiesPersistConfig, newsReducer),
+  hideReducer: persistReducer(hideReducerPersistConfig, hideReducer),
+  upvotedReducer: persistReducer(upvotedReducerPersistConfig, upvotedReducer)
 });
 
-export default rootReducer;
+export default persistReducer({ key: "primary", storage }, rootReducer);
